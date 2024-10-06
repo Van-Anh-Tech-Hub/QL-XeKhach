53ff97f5c5546dc20bbab6aa8b79d87693b0af45
Huynh Khang <huynhkhang0704@gmail.com>
2024-10-06 17:44:50 +0700

    Config folder

---
+++ WfmQLDichVuXeKhach/GUI/Frm_SignIn.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WfmQLDichVuXeKhach.GUI
+{
+    partial class Frm_SignIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(223, 256);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 37);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(533, 256);
+            button2.Name = "button2";
+            button2.Size = new Size(96, 37);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(223, 129);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(406, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(223, 172);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(406, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // Frm_SignIn
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Name = "Frm_SignIn";
+            Text = "Frm_SignIn";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+    }
+}
---
+++ WfmQLDichVuXeKhach/GUI/Frm_SignIn.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WfmQLDichVuXeKhach.GUI
+{
+    public partial class Frm_SignIn : Form
+    {
+        public Frm_SignIn()
+        {
+            InitializeComponent();
+        }
+    }
+}
