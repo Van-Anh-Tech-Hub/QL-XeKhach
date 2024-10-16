45fb1f57b0f06035b5351b22ec90e2598153c090
Văn Anh Vũ <vuvananh010203@gmail.com>
2024-10-16 16:33:48 +0700

    fix: update solution

--- QL_XeKhach/GUI/frmMain.Designer.cs
+++ QL_XeKhach/GUI/frmMain.Designer.cs
@@ -32,15 +32,15 @@
             // 
             // frmMain
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1440, 836);
+            this.ClientSize = new System.Drawing.Size(1920, 1029);
             this.IsMdiContainer = true;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "frmMain";
             this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
             this.Text = "frmMain";
-            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.ResumeLayout(false);
 
         }
